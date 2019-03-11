3ccf216195c11fa6b3e58dfd008a2250d9fb6839
tochsner <tobia.ochsner@hotmail.com>
2019-03-11 22:34:00 +0100

    Transformation

--- MNIST_classification_mine_transformation.py
+++ MNIST_classification_mine_transformation.py
@@ -12,6 +12,7 @@ import numpy as np
 def get_random_orthonormal_matrix(n):
     a = np.random.random(size=(n, n))
     q, r = np.linalg.qr(a)
+
     return q
 
 def train_model():
@@ -26,21 +27,21 @@ def train_model():
     mse = MeanSquaredCost()
 
     A = get_random_orthonormal_matrix(10)
-    A_t = A.T
+    A_t = np.linalg.inv(A)
 
     classifier = SimpleNeuronalNetwork((784, 100, 10), sigmoid_activation, sigmoid_derivation, mse)
 
     for e in range(epochs):
         for b in range(x_train.shape[0] // batch_size):
             for s in range(batch_size):
-                classifier.train_network(x_train[b * batch_size + s], A.dot(y_train[b * batch_size + s]))
+                classifier.train_network(x_train[b * batch_size + s], sigmoid_activation(A.dot(y_train[b * batch_size + s])))
                 
             classifier.apply_changes(lr, r)
 
         accuracy = 0        
 
         for s in range(x_test.shape[0]):
-            output = A_t.dot(classifier.get_output(x_test[s, :]))
+            output = A_t.dot(sigmoid_inverse(classifier.get_output(x_test[s, :])))
 
             if np.argmax(output) == np.argmax(y_test[s, : ]):
                 accuracy += 1            
